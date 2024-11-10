44a405b3d848297e4573e193a4a35ab1cd28771f
RSoup <90118880+RSoup@users.noreply.github.com>
2024-11-10 10:57:48 -0500

    Add files via upload

---
+++ PressureSensors.ino
@@ -0,0 +1,39 @@
+#include <CapacitiveSensor.h>
+
+
+CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+CapacitiveSensor   cs_4_6 = CapacitiveSensor(4,6);        // 10M resistor between pins 4 & 6, pin 6 is sensor pin, add a wire and or foil
+CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // 10M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
+CapacitiveSensor   cs_4_10 = CapacitiveSensor(4,10);
+CapacitiveSensor   cs_4_12 = CapacitiveSensor(4,12);
+
+void setup()                    
+{
+   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   Serial.begin(9600);
+}
+
+void loop()                    
+{
+    long start = millis();
+    long total1 =  cs_4_2.capacitiveSensor(30);
+    long total2 =  cs_4_6.capacitiveSensor(30);
+    long total3 =  cs_4_8.capacitiveSensor(30);
+    long total4 =  cs_4_10.capacitiveSensor(30);
+    long total5 =  cs_4_12.capacitiveSensor(30);
+
+    Serial.print(millis() - start);        // check on performance in milliseconds
+    Serial.print("\t");                    // tab character for debug windown spacing
+
+    Serial.print(total1);                  // print sensor output 1
+    Serial.print("\t");
+    Serial.print(total2);                  // print sensor output 2
+    Serial.print("\t");
+    Serial.print(total3);                // print sensor output 3
+    Serial.print("\t");
+    Serial.print(total4);
+    Serial.print("\t");
+    Serial.println(total5);
+
+    delay(10);                             // arbitrary delay to limit data to serial port 
+}
